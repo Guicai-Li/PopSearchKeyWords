ddc3476bc6a42edb13af1b8d9bbc13655731f2ea
Guicai-Li <guicai.li.china@gmail.com>
2015-07-30 12:48:34 +0800

    fix crash

--- PopSearchKeyWords/ViewController.m
+++ PopSearchKeyWords/ViewController.m
@@ -11,6 +11,8 @@
 
 @interface ViewController () <PopSearchKeyWordsDelegate>
 
+@property (nonatomic, strong) PopSearchKeyWords *keyWordsview;
+
 @end
 
 @implementation ViewController
@@ -19,13 +21,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor grayColor];
-    PopSearchKeyWords *keyWordsview = [[PopSearchKeyWords alloc] initWithFrame:CGRectMake(6, 70, self.view.bounds.size.width - 12, 300) onView:self.view];
-    keyWordsview.dataSource = @[@"Ubbie故事", @"宝贝", @"睡衣宝宝", @"天使", @"宝贝儿歌", @"儿童音乐", @"企鹅历险记", @"古诗词", @"童话故事", @"小优比"];
-    keyWordsview.view.backgroundColor = [UIColor whiteColor];
-    keyWordsview.view.layer.cornerRadius = 8.0f;
-    keyWordsview.view.layer.masksToBounds = YES;
-    keyWordsview.delegate = self;
-    [keyWordsview show];
+    
+    [self.keyWordsview show];
+}
+
+- (PopSearchKeyWords *)keyWordsview {
+    if (_keyWordsview == nil) {
+        _keyWordsview = [[PopSearchKeyWords alloc] initWithFrame:CGRectMake(6, 70, self.view.bounds.size.width - 12, 300) onView:self.view];
+        _keyWordsview.dataSource = @[@"Ubbie故事", @"宝贝", @"睡衣宝宝", @"天使", @"宝贝儿歌", @"儿童音乐", @"企鹅历险记", @"古诗词", @"童话故事", @"小优比"];
+        _keyWordsview.view.backgroundColor = [UIColor whiteColor];
+        _keyWordsview.view.layer.cornerRadius = 8.0f;
+        _keyWordsview.view.layer.masksToBounds = YES;
+        _keyWordsview.delegate = self;
+    }
+    return _keyWordsview;
 }
 
 - (void)clickedSearchKeyWord:(NSInteger)index {
